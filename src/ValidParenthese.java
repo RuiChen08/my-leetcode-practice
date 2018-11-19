5ea00183a3ef4cdba7fa268324f41df36fa2025c
Rui  Chen <rui.chen@anu.edu.au>
2018-11-19 16:16:53 +1100

    leetcode problem 20

--- src/ValidParenthese.java
+++ src/ValidParenthese.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+import java.util.LinkedList;
+import java.util.Stack;
 
 public class ValidParenthese {
+
+    public static void main(String[] args) {
+        ValidParenthese v = new ValidParenthese();
+        System.out.println(v.isValid("()"));
+
+    }
+
+//    public boolean isValid(String s) {
+//        if (s.length() == 0) return true;
+//        LinkedList<Character> heap = new LinkedList<>();
+//        for (int i = 0; i < s.length(); i++){
+//            char c = s.charAt(i);
+//            if (heap.size() != 0 && (c == ')' || c == ']' || c == '}')){
+//                if (!heap.removeLast().equals(c)) return false;
+//            } else {
+//                switch (c){
+//                    case '(': heap.add(')'); break;
+//                    case '[': heap.add(']'); break;
+//                    case '{': heap.add('}'); break;
+//                    default: return false;
+//                }
+//            }
+//        }
+//        return heap.size() == 0;
+//    }
+
+    public boolean isValid(String s){
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()){
+            switch (c){
+                case '(':
+                    st.push(')'); break;
+                case '[':
+                    st.push(']'); break;
+                case '{':
+                    st.push('}'); break;
+                default:
+                    if (st.isEmpty() || st.pop() != c) return false;
+            }
+        }
+        return st.isEmpty();
+    }
 }
