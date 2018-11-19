de5dba5dd55efa75a1e224ae98d8541f61b7e1a8
Rui Chen <rui.chen@anu.edu.au>
2018-11-19 18:05:01 +1100

    leetcode problem 21

--- src/RemoveNthNode.java
+++ src/RemoveNthNode.java
@@ -36,4 +36,27 @@ public class RemoveNthNode {
         return head;
     }
 
+
+    //Leetcode problem 21
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) return l1 == null ? l2 : l1;
+        boolean v = l1.val < l2.val;
+        ListNode sm = v ? l1 : l2, la = v ? l2 : l1;
+
+        while (sm != null && la != null) {
+            if (sm.next == null || sm.next.val > la.val) { ListNode temp = sm.next; sm.next = la; la = temp;}
+            sm = sm.next;
+        }
+
+        return v ? l1 : l2;
+//        if (l1.val < l2.val){
+//            l1.next = mergeTwoLists(l1.next, l2);
+//            return l1;
+//        } else{
+//            l2.next = mergeTwoLists(l1, l2.next);
+//            return l2;
+//        }
+
+    }
+
 }
