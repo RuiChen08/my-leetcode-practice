540d1991c05bf26a9200f8f69851510f33b7d0df
Rui  Chen <rui.chen@anu.edu.au>
2018-11-19 14:49:18 +1100

    leetcode 19 remove nth

--- src/RemoveNthNode.java
+++ src/RemoveNthNode.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class RemoveNthNode {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public static void main(String[] args) {
+        ListNode l = new ListNode(1);
+        l.next = new ListNode(2);
+        l.next.next = new ListNode(3);
+
+        RemoveNthNode r = new RemoveNthNode();
+        ListNode head = r.removeNthFromEnd(l, 3);
+        System.out.println(head.val);
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow = head, fast = head;
+
+        for (int i = 0; i < n; i++) {
+            if (fast.next == null) return head.next;
+            fast = fast.next;
+        }
+
+        while (fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return head;
+    }
+
 }
