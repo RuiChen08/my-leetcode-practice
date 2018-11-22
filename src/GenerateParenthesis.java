62da8520f4ae3aefaae369ae7a49a278066566d8
Rui Chen <rui.chen@anu.edu.au>
2018-11-22 23:35:17 +1100

    smart solution from leetcode discussion:

--- src/GenerateParenthesis.java
+++ src/GenerateParenthesis.java
@@ -7,31 +7,50 @@ public class GenerateParenthesis {
         System.out.println(g.generateParenthesis(3));
     }
 
+//    public List<String> generateParenthesis(int n) {
+//        List<List<String>> pss = new ArrayList<>(n + 1);
+//        for (int i = 0; i <= n; i++) {
+//            //
+//            List<String> ps = new LinkedList<>();
+//
+//            //
+//            if (i == 0)
+//                ps.add("");
+//            else {
+//                for (int c = 0; c < i; c++) {
+//
+//                    List<String> l1 = pss.get(c);
+//                    List<String> l2 = pss.get(i - c - 1);
+//
+//                    for (String s1 : l1) {
+//                        for (String s2 : l2) {
+//                            ps.add("(" + s1 + ")" + s2);
+//                        }
+//                    }
+//                }
+//            }
+//            pss.add(ps);
+//        }
+//        return pss.get(n);
+//    }
+
     public List<String> generateParenthesis(int n) {
-        List<List<String>> pss = new ArrayList<>(n + 1);
-        for (int i = 0; i <= n; i++) {
-            //
-            List<String> ps = new LinkedList<>();
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
 
-            //
-            if (i == 0)
-                ps.add("");
-            else {
-                for (int c = 0; c < i; c++) {
+    public void backtrack(List<String> list, String str, int open, int close, int max){
 
-                    List<String> l1 = pss.get(c);
-                    List<String> l2 = pss.get(i - c - 1);
-
-                    for (String s1 : l1) {
-                        for (String s2 : l2) {
-                            ps.add("(" + s1 + ")" + s2);
-                        }
-                    }
-                }
-            }
-            pss.add(ps);
+        if(str.length() == max*2){
+            list.add(str);
+            return;
         }
-        return pss.get(n);
+
+        if(open < max)
+            backtrack(list, str+"(", open+1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close+1, max);
     }
 
 }
--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -6,77 +6,97 @@ public class ThreeSum {
     public static void main(String[] args) {
         ThreeSum ts = new ThreeSum();
 //        System.out.println(ts.threeSum(new int[] {-1, 0, 1, 2, -1, -4}));
-//        System.out.println(ts.threeSum(new int[] {0,0,0,0}));
-        System.out.println(ts.threeSum(new int[] {1,2,-2,-1}));
+        System.out.println(ts.threeSum(new int[] {0,0,0,0}));
+//        System.out.println(ts.threeSum(new int[] {1,2,-2,-1}));
     }
 
 // Following codes from leetcode discussion formula, which has a better actual running time, but same time complexity
 // https://leetcode.com/problems/3sum/discuss/7380/Concise-O(N2)-Java-solution
 
+    public List<List<Integer>> threeSum(int[] num) {
+        Arrays.sort(num);
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < num.length-2; i++) {
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                        while (lo < hi && num[lo] == num[lo+1]) lo++;
+                        while (lo < hi && num[hi] == num[hi-1]) hi--;
+                        lo++; hi--;
+                    } else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
+                }
+            }
+        }
+        return res;
+    }
+
     // My codes:
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> solution = new LinkedList<>();
-
-        if (nums.length < 3) return solution;
-
-        for (int c = 0; c < nums.length; c++) {
-            if (c == 0 || nums[c] != nums[c-1]) {
-
-                HashMap<Integer, Integer> maps = new HashMap<>();
-                // Building the map for using hash for O(1) get
-                for (int i = c; i < nums.length; i++) maps.put(nums[i], i);
-                //
-                for (int i = c + 1; i < nums.length; i++){
-                    if (i == c + 1 || nums[i] != nums[i-1]) {
-                        int complement = 0 - nums[c] - nums[i];
-                        if (maps.containsKey(complement) && maps.get(complement) > i) {
-                            solution.add(Arrays.asList(nums[i], complement, nums[c]));
-                        }
-                        //
-                    }
-                }
-            }
-        }
-        return solution;
-    }
-
-
-    public List<List<Integer>> generalSum(int[] nums, int parts, int target, int head){
-        List<List<Integer>> solution = new ArrayList<>();
-        if (nums.length - head < parts) return solution;
-
-        if (parts == 2){
-            HashMap<Integer, Integer> maps = new HashMap<>();
-            // Building the map for using hash for O(1) get
-            for (int i = head; i < nums.length; i++) maps.put(nums[i], i);
-            //
-            for (int i = head; i < nums.length; i++){
-                if (i == head || nums[i] != nums[i-1]) {
-                    int complement = target - nums[i];
-                    List<Integer> tempList = new ArrayList<>();
-                    if (maps.containsKey(complement) && maps.get(complement) > i) {
-                        tempList.add(nums[i]);
-                        tempList.add(complement);
-                    }
-                    //
-                    if (tempList.size() != 0) solution.add(tempList);
-                }
-            }
-        } else {
-            // Note here I am not pretty sure
-            for (int i = head; i < nums.length; i++) {
-                if (i == head || nums[i] != nums[i-1]) {
-                    for (List<Integer> li : generalSum(nums, parts - 1, target - nums[i], i + 1)) {
-                        li.add(nums[i]);
-                        solution.add(li);
-                    }
-                }
-            }
-        }
-
-//      System.out.println(solution);
-        return solution;
-    }
+//    public List<List<Integer>> threeSum(int[] nums) {
+//        Arrays.sort(nums);
+//        List<List<Integer>> solution = new LinkedList<>();
+//
+//        if (nums.length < 3) return solution;
+//
+//        for (int c = 0; c < nums.length; c++) {
+//            if (c == 0 || nums[c] != nums[c-1]) {
+//
+//                HashMap<Integer, Integer> maps = new HashMap<>();
+//                // Building the map for using hash for O(1) get
+//                for (int i = c; i < nums.length; i++) maps.put(nums[i], i);
+//                //
+//                for (int i = c + 1; i < nums.length; i++){
+//                    if (i == c + 1 || nums[i] != nums[i-1]) {
+//                        int complement = 0 - nums[c] - nums[i];
+//                        if (maps.containsKey(complement) && maps.get(complement) > i) {
+//                            solution.add(Arrays.asList(nums[i], complement, nums[c]));
+//                        }
+//                        //
+//                    }
+//                }
+//            }
+//        }
+//        return solution;
+//    }
+//
+//
+//    public List<List<Integer>> generalSum(int[] nums, int parts, int target, int head){
+//        List<List<Integer>> solution = new ArrayList<>();
+//        if (nums.length - head < parts) return solution;
+//
+//        if (parts == 2){
+//            HashMap<Integer, Integer> maps = new HashMap<>();
+//            // Building the map for using hash for O(1) get
+//            for (int i = head; i < nums.length; i++) maps.put(nums[i], i);
+//            //
+//            for (int i = head; i < nums.length; i++){
+//                if (i == head || nums[i] != nums[i-1]) {
+//                    int complement = target - nums[i];
+//                    List<Integer> tempList = new ArrayList<>();
+//                    if (maps.containsKey(complement) && maps.get(complement) > i) {
+//                        tempList.add(nums[i]);
+//                        tempList.add(complement);
+//                    }
+//                    //
+//                    if (tempList.size() != 0) solution.add(tempList);
+//                }
+//            }
+//        } else {
+//            // Note here I am not pretty sure
+//            for (int i = head; i < nums.length; i++) {
+//                if (i == head || nums[i] != nums[i-1]) {
+//                    for (List<Integer> li : generalSum(nums, parts - 1, target - nums[i], i + 1)) {
+//                        li.add(nums[i]);
+//                        solution.add(li);
+//                    }
+//                }
+//            }
+//        }
+//
+////      System.out.println(solution);
+//        return solution;
+//    }
 
 }
