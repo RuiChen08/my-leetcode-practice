82f8c37de844e067230202e1c86712bdc3ba6c77
Rui Chen <rui.chen@anu.edu.au>
2018-11-25 00:00:47 +1100

    Leetcode problem 25

--- src/MergeKSortedLists.java
+++ src/MergeKSortedLists.java
@@ -84,5 +84,40 @@ public class MergeKSortedLists {
         return mid;
     }
 
+    public static void main(String[] args) {
+        MergeKSortedLists m = new MergeKSortedLists();
+        ListNode head = new ListNode(2);
+        ListNode next = new ListNode(3);
+        head.next = next;
+        m.reverseKGroup(head, 2);
+    }
+
+    //Leetcode problem 25
+    /*
+     *Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+     *
+     *k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+     *
+     *Example:
+     *
+     *Given this linked list: 1->2->3->4->5
+     *
+     *For k = 2, you should return: 2->1->4->3->5
+     *
+     *For k = 3, you should return: 3->2->1->4->5
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        ListNode pre, cur = head, nex = head.next;
+        for (int i = 1; i < k; i++) {
+            pre = cur;
+            cur = nex;
+            nex = cur.next;
+
+            cur.next = pre;
+        }
+        head.next = reverseKGroup(nex, k);
+        return cur;
+    }
 }
 
