653352cad357a1cff157242d0cce6ac527ffb2bc
Rui Chen <rui.chen@anu.edu.au>
2018-12-03 21:40:44 +0800

    Leetcode problem 37

--- src/ValidSudoku.java
+++ src/ValidSudoku.java
@@ -1,14 +1,18 @@
+import sun.awt.image.ImageWatched;
+
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ValidSudoku {
 
     public static void main(String[] args) {
         ValidSudoku v = new ValidSudoku();
-        System.out.println(v.isValidSudoku(
+        v.solveSudoku(
                 new char[][]
                         {{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}}
 
-        ));
+        );
     }
 
     public boolean isValidSudoku(char[][] board) {
@@ -27,4 +31,48 @@ public class ValidSudoku {
         return true;
     }
 
+    public boolean isValidSudokuMirror(char[][] board, int start, char c) {
+        int a = start / 9;
+        int b = start % 9;
+        int cubeS = (a / 3) * 27 + 3 * (b / 3);
+
+        for (int r = 0; r < 9; r++){
+
+            if (board[a][r] != '.' && board[a][r] == c) return false;
+            if (board[r][b] != '.' && board[r][b] == c) return false;
+            int cubeR = cubeS / 9 + r / 3;
+            int cubeC = cubeS % 9 + r % 3;
+            if (board[cubeR][cubeC] != '.' && board[cubeR][cubeC] == c) return false;
+        }
+        return true;
+    }
+
+    public void solveSudoku(char[][] board) {
+        solveSudokuHelper(board, 0);
+    }
+
+
+    public boolean solveSudokuHelper(char[][] board, int start) {
+        int r = start / 9;
+        int c = start % 9;
+
+        while (start < 81 && board[r][c] != '.') {
+            start++;
+            r = start / 9;
+            c = start % 9;
+        }
+
+        if (start == 81) return true;
+
+        for (char i = '1'; i <= '9'; i++){
+            if (isValidSudokuMirror(board, start, i)){
+                board[r][c] = i;
+                if (solveSudokuHelper(board, start + 1)) return true;
+                else board[r][c] = '.';
+            }
+        }
+
+        return false;
+    }
 }
+
