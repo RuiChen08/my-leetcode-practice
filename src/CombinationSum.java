ab21f2dd7eb0f87b49bb067e8ed08b9a0f94a6fd
Rui Chen <rui.chen@anu.edu.au>
2018-12-05 20:59:17 +0800

    Leetcode problem 40

--- src/CombinationSum.java
+++ src/CombinationSum.java
@@ -4,7 +4,7 @@ public class CombinationSum {
 
     public static void main(String[] args) {
         CombinationSum c = new CombinationSum();
-        System.out.println(c.combinationSum(new int[] {2,3,6,7}, 7));
+        System.out.println(c.combinationSum2(new int[] {2,5,2,2,2,1,2}, 5));
 
     }
 
@@ -31,4 +31,30 @@ public class CombinationSum {
         }
     }
 
+    // Leetcode problem 40
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> answers = new LinkedList<>();
+
+        combinationSumHelper2(candidates, target, 0, answers, new LinkedList<>(), new int[candidates.length]);
+        return answers;
+    }
+
+    public void combinationSumHelper2(int[] candidates, int target, int cursor, List<List<Integer>> answers, LinkedList<Integer> answer, int[] indicate) {
+        if (target < 0) return;
+        for (int i = cursor; i < candidates.length; i++){
+            if (i != 0 && indicate[i-1] == 0 && candidates[i] == candidates[i - 1]) continue;
+            if (target - candidates[i] > 0) {
+                answer.add(candidates[i]);
+                indicate[i] = 1;
+                combinationSumHelper2(candidates, target - candidates[i], i + 1, answers, answer, indicate);
+                answer.remove(answer.size() - 1);
+                indicate[i] = 0;
+            }else if (target - candidates[i] == 0) {
+                List<Integer> ll = new LinkedList<>(answer);
+                ll.add(candidates[i]);
+                answers.add(ll);
+            }
+        }
+    }
 }
