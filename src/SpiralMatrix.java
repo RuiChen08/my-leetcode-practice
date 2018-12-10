f0d945aa9a570f0e7f87f09ac1c8497a421e4e74
Rui Chen <rui.chen@anu.edu.au>
2018-12-10 18:07:46 +0800

    Leetcode problem 59

--- src/SpiralMatrix.java
+++ src/SpiralMatrix.java
@@ -36,4 +36,37 @@ public class SpiralMatrix{
             out.add(matrix[r][c]);
         }
     }
+
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+
+        performSpiral2(matrix, 0, 0, n, 1);
+        return matrix;
+    }
+
+    public void performSpiral2(int[][] matrix, int startr, int startc, int n, int count){
+        int r = startr, c = startc;
+
+        for (; c < startc + n; c++){
+            matrix[r][c] = count++;
+        }
+        c--;
+        r++;
+        for (; r < startr + n; r++){
+            matrix[r][c] = count++;
+        }
+        r--;
+        c--;
+        for (; r != startr && c >= startc; c--){
+            matrix[r][c] = count++;
+        }
+        c++;
+        r--;
+        for (;c != startc + n && r > startr; r--){
+            matrix[r][c] = count++;
+        }
+        if (n > 2) {
+            performSpiral2(matrix, r + 1, c + 1, n - 2, count);
+        }
+    }
 }
